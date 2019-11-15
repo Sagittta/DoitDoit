bd5198a96cb947ff631fd5bbcf98e85cd9f577f1
Sagittta <js1verj@gmail.com>
2019-11-15 22:03:00 +0900

    Zilla Slab Highlight Font, SelectSubjectActivity, button custom, gradient background

---
+++ app/src/main/java/kr/hs/emirim/sagittta/DoitDoit/SelectDateActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.sagittta.DoitDoit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_date);
+    }
+}
