ce3283b92a21c3f4730a46228f1b0df4d158d1fd
Sagittta <js1verj@gmail.com>
2019-11-19 18:37:51 +0900

    Tab Widget

--- app/src/main/java/kr/hs/emirim/sagittta/DoitDoit/SelectActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/DoitDoit/SelectActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.sagittta.DoitDoit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
+        tabHost1.setup();
+
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("TAB 1");
+        tabHost1.addTab(ts1);
+
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("TAB 2");
+        tabHost1.addTab(ts2);
+
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("TAB 3");
+        tabHost1.addTab(ts3);
     }
 }
