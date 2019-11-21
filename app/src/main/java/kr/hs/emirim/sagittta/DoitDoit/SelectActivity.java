6313be941b019d21e5fc43ad2697459b67ab2853
Sagittta <js1verj@gmail.com>
2019-11-21 19:45:36 +0900

    recyclerView item click event - Toast

--- app/src/main/java/kr/hs/emirim/sagittta/DoitDoit/SelectActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/DoitDoit/SelectActivity.java
@@ -1,15 +1,20 @@
 package kr.hs.emirim.sagittta.DoitDoit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -64,6 +69,19 @@ public class SelectActivity extends AppCompatActivity {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(sRecyclerView.getContext(), sLinearLayoutManager.getOrientation());
         sRecyclerView.addItemDecoration(dividerItemDecoration);
 
+        sRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), sRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Subjects subj = sList.get(position);
+                Toast.makeText(getApplicationContext(), subj.getSubject(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
         Button btAddSubject = (Button) findViewById(R.id.btAddSubject);
         //TODO 과목 입력받는 팝업창 띄우기
         btAddSubject.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +94,54 @@ public class SelectActivity extends AppCompatActivity {
                 sAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private SelectActivity.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final SelectActivity.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+        }
+
     }
 
 //    private void changeView(int index) {
